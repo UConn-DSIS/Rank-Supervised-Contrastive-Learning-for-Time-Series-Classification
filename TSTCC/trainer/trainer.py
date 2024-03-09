6421d5214421444eacfa1860fff19d48f7117dc7
Qianying Ren <89426797+HappyDay1998@users.noreply.github.com>
2024-03-09 16:35:29 -0500

    Add files via upload

---
+++ TSTCC/trainer/trainer.py
@@ -0,0 +1,155 @@
+import os
+import sys
+
+sys.path.append("..")
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.metrics import precision_score, f1_score
+from models.loss import NTXentLoss
+
+
+
+def Trainer(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, train_dl, valid_dl, test_dl, device, logger, config, experiment_log_dir, training_mode,dataset,seed):
+    # Start training
+    logger.debug("Training started ....")
+
+    criterion = nn.CrossEntropyLoss()
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(model_optimizer, 'min')
+
+    for epoch in range(1, config.num_epoch + 1):
+        # Train and validate
+        train_loss, train_acc = model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, criterion, train_dl, config, device, training_mode)
+        valid_loss, valid_acc,valid_precision,valid_f1, _, _ = model_evaluate(model, temporal_contr_model, valid_dl, device, training_mode)
+        if training_mode != 'self_supervised':  # use scheduler in all other modes.
+            scheduler.step(valid_loss)
+
+        logger.debug(f'\nEpoch : {epoch}\n'
+                     f'Train Loss     : {train_loss:.4f}\t | \tTrain Accuracy     : {train_acc:2.4f}\n'
+                     f'Valid Loss     : {valid_loss:.4f}\t | \tValid Accuracy     : {valid_acc:2.4f}')
+   # load_from = os.path.join(os.path.join('/home/qir21001/TS-TCC-main/',logs_save_dir, experiment_description, run_description, f"self_supervised_seed_{SEED}", "saved_models"))
+    #chkpoint = torch.load(os.path.join(load_from, "ckp_last_%s_%d.pt"%(args.dataset,args.seed)), map_location=device)
+    os.makedirs(os.path.join(experiment_log_dir, "saved_models"), exist_ok=True)
+    chkpoint = {'model_state_dict': model.state_dict(), 'temporal_contr_model_state_dict': temporal_contr_model.state_dict()}
+    torch.save(chkpoint, os.path.join(experiment_log_dir, "saved_models", 'ckp_last_%s_%d.pt'%(dataset,seed)))
+
+    if training_mode != "self_supervised":  # no need to run the evaluation for self-supervised mode.
+        # evaluate on the test set
+        logger.debug('\nEvaluate on the Test set:')
+        test_loss, test_acc,test_precision,test_f1, _, _ = model_evaluate(model, temporal_contr_model, test_dl, device, training_mode)
+        logger.debug(f'Test loss      :{test_loss:0.4f}\t | Test Accuracy      : {test_acc:0.4f}\t | Test Precision Score       : {test_precision:0.4f}\t| Test F1 score    :{test_f1:0.4f}')
+
+    logger.debug("\n################## Training is Done! #########################")
+
+
+def model_train(model, temporal_contr_model, model_optimizer, temp_cont_optimizer, criterion, train_loader, config, device, training_mode):
+    total_loss = []
+    total_acc = []
+    model.train()
+    temporal_contr_model.train()
+
+    for batch_idx, (data, labels, aug1, aug2) in enumerate(train_loader):
+        # send to device
+        data, labels = data.float().to(device), labels.long().to(device)
+        aug1, aug2 = aug1.float().to(device), aug2.float().to(device)
+
+        # optimizer
+        model_optimizer.zero_grad()
+        temp_cont_optimizer.zero_grad()
+
+        if training_mode == "self_supervised":
+            predictions1, features1 = model(aug1)
+            predictions2, features2 = model(aug2)
+
+            # normalize projection feature vectors
+            features1 = F.normalize(features1, dim=1)
+            features2 = F.normalize(features2, dim=1)
+
+            temp_cont_loss1, temp_cont_lstm_feat1 = temporal_contr_model(features1, features2)
+            temp_cont_loss2, temp_cont_lstm_feat2 = temporal_contr_model(features2, features1)
+
+            # normalize projection feature vectors
+            zis = temp_cont_lstm_feat1 
+            zjs = temp_cont_lstm_feat2 
+
+        else:
+            output = model(data)
+
+        # compute loss
+        if training_mode == "self_supervised":
+            lambda1 = 1
+            lambda2 = 0.7
+            nt_xent_criterion = NTXentLoss(device, config.batch_size, config.Context_Cont.temperature,
+                                           config.Context_Cont.use_cosine_similarity)
+            loss = (temp_cont_loss1 + temp_cont_loss2) * lambda1 +  nt_xent_criterion(zis, zjs) * lambda2
+            
+        else: # supervised training or fine tuining
+            predictions, features = output
+            loss = criterion(predictions, labels)
+            total_acc.append(labels.eq(predictions.detach().argmax(dim=1)).float().mean())
+
+        total_loss.append(loss.item())
+        loss.backward()
+        model_optimizer.step()
+        temp_cont_optimizer.step()
+
+    total_loss = torch.tensor(total_loss).mean()
+
+    if training_mode == "self_supervised":
+        total_acc = 0
+    else:
+        total_acc = torch.tensor(total_acc).mean()
+    return total_loss, total_acc
+
+
+def model_evaluate(model, temporal_contr_model, test_dl, device, training_mode):
+    model.eval()
+    temporal_contr_model.eval()
+
+    total_loss = []
+    total_acc = []
+    precisions = []
+    f1_scores = []
+    criterion = nn.CrossEntropyLoss()
+    outs = np.array([])
+    trgs = np.array([])
+
+    with torch.no_grad():
+        for data, labels, _, _ in test_dl:
+            data, labels = data.float().to(device), labels.long().to(device)
+
+            if training_mode == "self_supervised":
+                pass
+            else:
+                output = model(data)
+
+            # compute loss
+            if training_mode != "self_supervised":
+                predictions, features = output
+                loss = criterion(predictions, labels)
+                total_acc.append(labels.eq(predictions.detach().argmax(dim=1)).float().mean())
+                total_loss.append(loss.item())
+                prediction = predictions.detach().argmax(dim=1)
+                precision = precision_score(labels.cpu().numpy(),prediction.cpu().numpy(),average='macro')
+                f1 = f1_score(labels.cpu().numpy(),prediction.cpu().numpy(),average='macro')
+                precisions.append(precision)
+                f1_scores.append(f1)
+            if training_mode != "self_supervised":
+                pred = predictions.max(1, keepdim=True)[1]  # get the index of the max log-probability
+                outs = np.append(outs, pred.cpu().numpy())
+                trgs = np.append(trgs, labels.data.cpu().numpy())
+
+    if training_mode != "self_supervised":
+        total_loss = torch.tensor(total_loss).mean()  # average loss
+    else:
+        total_loss = 0
+    if training_mode == "self_supervised":
+        total_acc = 0
+        return total_loss, total_acc, [], [],[],[]
+    else:
+        total_acc = torch.tensor(total_acc).mean()  # average acc
+        total_precision = torch.tensor(precisions).mean()
+        total_f1 = torch.tensor(f1_scores).mean()
+    return total_loss, total_acc,total_precision,total_f1,outs, trgs
